7c2976c8008476d84e870c52d8761ee786b19384
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-17 06:52:52 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -14,7 +14,7 @@ hello_t(array)
 # call your method here!
 
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+hello_t(["Tim", "Tom", "Jim"]).each do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
